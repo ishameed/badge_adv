9605122b7631cb9dee8d46b70a4016e689e3e580
ishameed <ishameed@umich.edu>
2019-11-05 14:26:03 -0500

    m

--- rpn.py
+++ rpn.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 def calculate(arg):
-    pass
+    stack = list()
+    for token in arg.split():
+        print(token)
+
+        if token == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+
+        else:
+            stack.append(int(token))
+        print(stack)
+    return stack.pop()
+
+
 def main():
     while True:
         calculate(input("rpn calc> "))
+    
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
     main()
