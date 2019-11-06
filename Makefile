fd718d0fcf87b16fa6400fed3663c0437bbdb8f3
ishameed <ishameed@umich.edu>
2019-11-05 20:14:03 -0500

    first commit, added travis yml file

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-Test:
+test:
 	python3 -m unittest
 
 
--- rpn.py
+++ rpn.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
+import readline
+
 def calculate(arg):
     stack = list()
     for token in arg.split():
         print(token)
 
         if token == '+':
-            arg1 = stack.pop()
             arg2 = stack.pop()
+            arg1 = stack.pop()
             result = arg1 + arg2
+            stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
             stack.append(result)
 
         else:
             stack.append(int(token))
         print(stack)
+
+    if len(stack) != 1:
+        raise TypeError('malformed input')
     return stack.pop()
 
 
 def main():
     while True:
         calculate(input("rpn calc> "))
-    
+
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
     main()
